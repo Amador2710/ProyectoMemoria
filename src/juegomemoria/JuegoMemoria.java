99b91522751d790a43ecfb13dc15c36c2c608eac
Kendall <kendallamador12@gmail.com>
2022-04-19 18:51:22 -0600

    Comentarios de JuegoMmemoria

--- src/juegomemoria/JuegoMemoria.java
+++ src/juegomemoria/JuegoMemoria.java
@@ -1,7 +1,7 @@
 package juegomemoria;
 
 /**
- * Librerias
+ * Librerias usadas
  */
 
 import java.awt.*;
@@ -49,7 +49,7 @@ public class JuegoMemoria extends JFrame implements ActionListener{
         this.numaleatorios();
 
         /**
-         * Genera la matriz para las imá    genes
+         * Genera la matriz par las imágenes
          */
         matriz = new JLabel[4][5];
         for (int i = 0; i < 4; i++) {
@@ -57,19 +57,19 @@ public class JuegoMemoria extends JFrame implements ActionListener{
                 matriz[i][j] = new JLabel();
                 matriz[i][j].setSize(matriz[i][j].getWidth(), matriz[i][j].getHeight());
                 /**
-                 * Se les pone un tamaño predeterminado a las imágenes para que no se vean mal
+                 * Se les pone un tamaño predeterminado a las imágenes para que se vean bien
                  */
                 matriz[i][j].setBounds(350+(j*125),30+(i*156), 125, 156);
                 /**
-                 * Se declara las imágenes del 0 al 10
+                 * Se toman las imágenes con el nombre del 0 al 10
                  */
                 matriz[i][j].setIcon(new ImageIcon("src/imagenes/"+mat2[i][j]+".JPG"));
                 /**
-                 * Se colocan las matrices visibles para que se vean las imágenes
+                 * Se hacen las matrices visibles para que se muestren las imágenes
                  */
                 matriz[i][j].setVisible(true);
                 /**
-                 * se añade la imagen 0 que es la del logo de los vengadores
+                 * Aquí se añade la imagen 0 que es la del logo de los vengadores
                  */
                 panel.add(matriz[i][j],0);
 
@@ -82,8 +82,7 @@ public class JuegoMemoria extends JFrame implements ActionListener{
         seg = 0;
         min = 0;
 
-        //este lo colocamos para podermo mostrar el tiempo que
-        //transcurre durante el juego
+
         tiempo = new Timer (1000, new ActionListener()
         {
 
@@ -96,14 +95,17 @@ public class JuegoMemoria extends JFrame implements ActionListener{
                     seg=0;
                 }
 
-                //declaramos en una variable cronometro el tiempo que transcurre
+                /**
+                 * se hace una variable para poner un cronometro
+                 */
                 cronometro.setText(min+":"+seg);
             }});
 
-        //declaramos en la variable espera la cual es otro tiempo el cual lo utilizamos
-        //para colocar un tiempo a la hora de que las cartas se voltean
+        /**
+         * Se hace la variable para la duración de las cartas en darse la vuelta
+         */
         consegund = 0;
-        espera = new Timer (1000, new ActionListener()
+        espera = new Timer (200, new ActionListener()
         {
 
             public void actionPerformed(ActionEvent e)
@@ -116,7 +118,9 @@ public class JuegoMemoria extends JFrame implements ActionListener{
         ban=0;
         ban1=0;
 
-        //evento de clic en la cartas
+        /**
+         * Aquí va el evento para el clic de las cartas
+         */
         contador = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 5; j++) {
@@ -125,9 +129,11 @@ public class JuegoMemoria extends JFrame implements ActionListener{
                         for (int k = 0; k < 4; k++) {
                             for (int l = 0; l < 5; l++) {
                                 if(e.getSource() == matriz[k][l]){
-                                    // System.out.println(k+" "+l);
 
-                                    //cuando se da click a la carta esta se volteara
+
+                                    /**
+                                     * Cuando se le da al clic la carta se dará la vuelta
+                                     */
                                     if(mat2[k][l] == 0 && contador !=2){
                                         mat2[k][l] = mat[k][l];
                                         matriz[k][l].setIcon(new ImageIcon("src/imagenes/"+mat2[k][l]+".JPG"));
@@ -141,7 +147,7 @@ public class JuegoMemoria extends JFrame implements ActionListener{
                                             anposy = l;
                                         }
 
-                                        //tiempo que se tarda en dar vuelta
+
                                         espera2 = new Timer (500, new ActionListener()
                                         {
 
@@ -155,7 +161,9 @@ public class JuegoMemoria extends JFrame implements ActionListener{
                                                     espera.stop();
                                                     consegund = 0;
 
-                                                    //Desaparecen las cartas que son iguales y deja las que aun no se encuentran
+                                                    /**
+                                                     * Desaparecerán las cartas iguales y las otras se quedan
+                                                     */
                                                     if(mat2[acposx][acposy]==mat2[anposx][anposy]){
 
                                                         mat2[acposx][acposy] = -1;
@@ -163,7 +171,7 @@ public class JuegoMemoria extends JFrame implements ActionListener{
                                                         matriz[acposx][acposy].setIcon(new ImageIcon("src/imagenes/"+mat2[acposx][acposy]+".JPG"));
                                                         matriz[anposx][anposy].setIcon(new ImageIcon("src/imagenes/"+mat2[anposx][anposy]+".JPG"));
                                                         contador=0;
-                                                        //gano si toda la mat2 es -1
+
                                                         int acum = 0;
                                                         for (int m = 0; m < 4; m++) {
                                                             for (int n = 0; n < 5; n++) {
@@ -171,9 +179,9 @@ public class JuegoMemoria extends JFrame implements ActionListener{
                                                                     acum++;
                                                             }
                                                         }
-                                                        //cuando no se encuentre ninguna para entonces aparecera
-                                                        //un mensaje diciendo que gano y ejecutando automaticamente
-                                                        //la ventana Recordjugador
+                                                        /**
+                                                         * Cuando se encuentren todas las cartas saltará un mensaje de que se ganó
+                                                         */
                                                         if(acum == 20){
                                                             JOptionPane.showMessageDialog(panel, "FELICIDADES GANASTE");
 
@@ -189,14 +197,13 @@ public class JuegoMemoria extends JFrame implements ActionListener{
                                                     }
                                                     for (int m = 0; m < 4; m++) {
                                                         for (int n = 0; n < 5; n++) {
-                                                            //se coloca el valor -1 a las cartas pares
                                                             if(mat2[m][n]!=0 && mat2[m][n]!=-1){
                                                                 mat2[m][n] = 0;
                                                                 matriz[m][n].setIcon(new ImageIcon("src/imagenes/"+mat2[m][n]+".JPG"));
                                                                 contador=0;
 
                                                             }
-                                                            System.out.println("Se encontró un par");
+                                                            System.out.println("Carta");
 
                                                         }
 
@@ -230,10 +237,12 @@ public class JuegoMemoria extends JFrame implements ActionListener{
         componentes();
         lblfecha.setText(fecha());
         hora();
-        //hilo = new Thread(this);
+
     }
 
-    //este metodo va obtener la hora actual del despositivo
+    /**
+     * para que tome la hora de la compu
+     */
     private void hora(){
         Calendar calendario = new GregorianCalendar();
         hora= calendario.get(Calendar.HOUR_OF_DAY);
@@ -241,7 +250,10 @@ public class JuegoMemoria extends JFrame implements ActionListener{
         lblhora.setText(hora + ":" + minutos);
     }
 
-    // este metodo obtiene la fecha del dia mediante el dispositivo
+    /**
+     *
+     * Toma la fecha de la compu
+     */
     private String fecha(){
         Date fecha = new Date();
         SimpleDateFormat formatofecha = new SimpleDateFormat("dd/MM/YYYY");
@@ -249,10 +261,9 @@ public class JuegoMemoria extends JFrame implements ActionListener{
 
     }
 
-    //este metodo se hace para obtener aleatoriamente las cartas
-    //cada vez que se inicie el juego las cartas apareceran en
-    //diferentes lugares.
-    //tambien para obtener dos veces la misma carta
+    /**
+     * Cada vez que se inicia una partida nueva las cartas cambian de posición
+     */
     private void numaleatorios(){
         int acumulador = 0;
         for (int i = 0; i < 4; i++)
@@ -275,7 +286,9 @@ public class JuegoMemoria extends JFrame implements ActionListener{
                             }
                         }
                     }
-                    //esto se coloca para que solo se duplique dos veces la carta
+                    /**
+                     * Duplica las cartas
+                     */
                     if(acumulador == 3){
                         mat[i][j] = ran.nextInt(10)+1;
                     }
@@ -290,8 +303,9 @@ public class JuegoMemoria extends JFrame implements ActionListener{
     }
 
 
-
-    //estos son los componentes del programa
+    /**
+     * Los labels del juego
+     */
     private void componentes(){
         etiqueta = new JLabel("Jugador: ");
         etiqueta.setBounds(40,40,150,40);
@@ -308,7 +322,7 @@ public class JuegoMemoria extends JFrame implements ActionListener{
         etiqueta.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 15));
         panel.add(etiqueta);
 
-        //en este JLabel aparecera la hora transcurrida durante el juego
+
         cronometro = new JLabel();
         cronometro.setBounds(135,80,150,40);
         cronometro.setFont(new Font("Times New Roman", Font.PLAIN, 20));
@@ -319,7 +333,7 @@ public class JuegoMemoria extends JFrame implements ActionListener{
         etiqueta.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 15));
         panel.add(etiqueta);
 
-        //en este JLabel se mostrar la fecha
+
         lblfecha = new JLabel();
         lblfecha.setBounds(135,160,150,40);
         lblfecha.setFont(new Font("Times New Roman", Font.PLAIN, 20));
@@ -330,13 +344,13 @@ public class JuegoMemoria extends JFrame implements ActionListener{
         etiqueta.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 15));
         panel.add(etiqueta);
 
-        //en este JLabel se mostrara la hora
+
         lblhora = new JLabel();
         lblhora.setBounds(135,120,150,40);
         lblhora.setFont(new Font("Times New Roman", Font.PLAIN, 20));
         panel.add(lblhora);
 
-        //este boton reiniciara el juego
+
         reiniciar = new JButton("Reiniciar partida");
         reiniciar.setBounds(80,560,200,40);
         reiniciar.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 15));
@@ -346,12 +360,15 @@ public class JuegoMemoria extends JFrame implements ActionListener{
     }
 
 
-    //estos son los eventos de accion
+    /**
+     * Eventos del juego
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        //se coloca que el boton reinicia mostrara un mensaje preguntando si esta segurdo de querea hacerlo
-        //si es si, se reiniciara desde el inicio y sino pues seguira en la partida
+        /**
+         * Para el botón de reiniciar partida
+         */
         if(e.getSource() == reiniciar){
             if (JOptionPane.showConfirmDialog(rootPane, "¿Quiere reiniciar la partida?\n Perderá todo el progreso realizado.",
                     "Reiniciar", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE) == JOptionPane.YES_OPTION)
